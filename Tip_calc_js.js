5b276c1035b56f6b8cf946f618f1f57bed40c973
Aryan Tele <58328909+AryanTele@users.noreply.github.com>
2023-12-27 01:24:30 +0530

    Add: 1. Choose tip functionality 2. showBill() function 3. made total_per_person global
    Delete:
    1. Redundant and simple comment (over cluttering)
    2. removed total_per_person from CalcBill()

--- Tip_calc_js.js
+++ Tip_calc_js.js
@@ -1,58 +1,53 @@
 //global access
 
-let billtotal_ip_div = document.getElementById('billtotal_ip')
-let tip_ip_div = document.getElementById('tip_ip')
-let no_of_ppl_div = document.getElementById('no_of_ppl')
-let total_per_person_div = document.getElementById('total_per_person')
+let billtotal_ip_div = document.getElementById("billtotal_ip");
+let tip_ip_div = document.getElementById("tip_ip");
+let no_of_ppl_div = document.getElementById("no_of_ppl");
+let total_per_person_div = document.getElementById("total_per_person");
+let buttonSuggestion = document.querySelectorAll(".suggestion");
+let no_of_ppl = Number(no_of_ppl_div.innerText);
+let tip = 0;
+let total_per_person = 0;
 
+// listen for each button clicked
+buttonSuggestion.forEach(function (button) {
+  button.addEventListener("click", function (event) {
+    // Retrieve the value of the clicked button
+    tip = event.target.value;
+    tip_ip_div.value = tip;
+    tip = tip / 100;
+    // called calcBill here to update bill value after adding tip
+    CalcBill();
+  });
+});
 
-let no_of_ppl = Number(no_of_ppl_div.innerText)
-
-
-const CalcBill = () =>{
-
-    //get bill from user
-    const bill = Number(billtotal_ip_div.value)
-    
-
-    //get tip from user
-    let tip = Number(tip_ip_div.value)/100
-
-    //calculate the tip amount
-    const tip_amount = bill*tip
-
-    //calculate the total amount 
-    const total_amount = bill+tip_amount
-
-    //calculate the total per person
-    const total_per_person = total_amount/no_of_ppl
-
-    //display the total
-    total_per_person_div.innerText = `$${total_per_person.toFixed(2)}` //.toFixed() sets the no. of digits after decimal 
-    //.toLocaleString('en-Us') --> 3,300, 1,20,000
-
+function showBill() {
+  /* display the total
+    .toLocaleString('en-Us') --> 3,300, 1,20,000
+    .toFixed() sets the no. of digits after decimal */
+  total_per_person_div.innerText = `$${total_per_person.toFixed(2)}`;
 }
 
-const increasePeople = () =>{
+const CalcBill = () => {
+  const bill = Number(billtotal_ip_div.value);
+  const tip_amount = bill * tip;
+  const total_amount = bill + tip_amount;
+  total_per_person = total_amount / no_of_ppl;
+  showBill();
+};
 
-    //at every click on '+' increment the no of people
-    no_of_ppl+=1 
+const increasePeople = () => {
+  no_of_ppl += 1;
+  no_of_ppl_div.innerText = no_of_ppl;
+  CalcBill();
+};
 
-    //display the updates on the dom
-    no_of_ppl_div.innerText = no_of_ppl
-    CalcBill()
-}
-
-const decreasePeople = () =>{
-
-    if(no_of_ppl<=1){
-        alert("Solo diner? No tip split! Add a buddy or treat yo'self! ️")
-        return
-    }
-    //at every click on '-' decrement the no of people
-    no_of_ppl-=1 
-
-    //display the updates on the dom
-    no_of_ppl_div.innerText = no_of_ppl
-    CalcBill()
-}
+const decreasePeople = () => {
+  if (no_of_ppl <= 1) {
+    alert("Solo diner? No tip split! Add a buddy or treat yo'self! ️");
+    return;
+  }
+  no_of_ppl -= 1;
+  no_of_ppl_div.innerText = no_of_ppl;
+  CalcBill();
+};
